6042692eb7c25dacc1e6000d395357bf3cae6997
s.tapia-sontag <s.tapia-sontag@wolterskluwer.com>
2019-03-25 13:38:09 +0100

    cleanup

--- src/main/java/com/wk/autoverleihMVC/AutoMVCApplication.java
+++ src/main/java/com/wk/autoverleihMVC/AutoMVCApplication.java
@@ -1,12 +1,21 @@
 package com.wk.autoverleihMVC;
 
+import com.wk.autoverleihMVC.model.Booking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.lang.reflect.Field;
 
 @SpringBootApplication
 public class AutoMVCApplication {
 
-	public static void main(String[] args) {
+//TEST
+	public static void main(String... args) {
 		SpringApplication.run(AutoMVCApplication.class, args);
+		for (Field field : Booking.class.getDeclaredFields()) {
+			if (field.isAnnotationPresent(org.springframework.format.annotation.DateTimeFormat.class)) {
+				System.out.println("The field " + field + " has DateTimeFormat annotation.");
+			}else System.out.println(field);
+		}
 	}
 }
